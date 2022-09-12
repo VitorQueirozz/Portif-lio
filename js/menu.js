352213e9539bba23c1ae2dd4d5e1b8d1ba49f309
Vitor Manoel <104446767+VitorQueirozz@users.noreply.github.com>
2022-09-12 16:03:24 -0300

    commit

--- js/menu.js
+++ js/menu.js
@@ -2,9 +2,9 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu') 
     if(menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open')
-        document.querySelector('.icon').src = "../imagem/menu.svg"
+        document.querySelector('.icon').src = "imagem/menu.svg"
     } else {
         menuMobile.classList.add('open')
-        document.querySelector('.icon').src = "../imagem/close.svg"
+        document.querySelector('.icon').src = "imagem/close.svg"
     }
 }
